3ffc801119191b15cb79f340414dcf4f0bea50ce
Lucas <denglucasyijin@gmail.com>
2023-04-12 17:40:20 +0800

    donee again

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: djin <djin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:01:12 by djin              #+#    #+#             */
-/*   Updated: 2023/04/12 15:05:30 by djin             ###   ########.fr       */
+/*   Updated: 2023/04/12 17:39:52 by djin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -29,7 +29,7 @@ char	*ft_strncpy(char	*dest, char	*src, unsigned	int n)
 	return (dest);
 }
 
-int main(void)
+/*int main(void)
 {
 	char	original_str[] = "Helloooo";
 	char	new_str[100];
@@ -39,4 +39,4 @@ int main(void)
 		ft_strncpy(new_str, original_str, n);
 	printf("New string is: %s\n", new_str);
 	return (0);
-}
+}*/
