80439ff4980160c16a6d5449a82ffaaa0e69b9a6
Lucas <denglucasyijin@gmail.com>
2023-04-12 15:06:04 +0800

    4 try

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: djin <djin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:01:12 by djin              #+#    #+#             */
-/*   Updated: 2023/04/11 20:47:35 by djin             ###   ########.fr       */
+/*   Updated: 2023/04/12 15:05:30 by djin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,27 +16,27 @@ char	*ft_strncpy(char	*dest, char	*src, unsigned	int n)
 	unsigned int	i;
 
 	i = 0;
-	while ((i < n) && (dest != (void *)0))
+	while (i < n && src[i] != '\0')
 	{
-		src[i] = dest[i];
+		dest [i] = src [i];
 		i++;
 	}
-	while ((i < n) && (dest != (void *)0))
+	while (i < n)
 	{
-		src[i] = '\0';
+		dest [i] = '\0';
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
 
-/*int main(void)
+int main(void)
 {
 	char	original_str[] = "Helloooo";
-	char	new_str[] = "you";
+	char	new_str[100];
+	unsigned int	n = 4;
 
 	printf("Original string is: %s\n", original_str);
-		ft_strncpy(new_str, original_str, 2);
+		ft_strncpy(new_str, original_str, n);
 	printf("New string is: %s\n", new_str);
 	return (0);
-}*/
+}
